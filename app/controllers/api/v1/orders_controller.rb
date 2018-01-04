d854b1399a305bc5ffdadca52098dff524865958
Orest Kostiuk <xxmolekula@gmail.com>
2018-01-04 18:06:43 +0200

    add search orders

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,8 +1,12 @@
 class Api::V1::OrdersController < ApplicationController
   before_action :find_order, except: :index
   def index
-    @orders = Order.where(restaurant_id: current_user.restaurants.first.id).search (params[:letters])
-    render json: @orders
+    # @orders = Order.where(restaurant_id: current_user.restaurants.first.id).search (params[:letters])
+    @orders = Order.search(params[:number])
+    @d = @orders.select { |a| a.restaurant_id == current_user.restaurants.first.id}
+
+
+    render json: @d
   end
 
   def show
