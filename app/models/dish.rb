1103e31632691441b57d614152d80447f8669e97
Orest Kostiuk <xxmolekula@gmail.com>
2018-01-11 18:35:24 +0200

    add image

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,4 +1,5 @@
 class Dish < ApplicationRecord
+  mount_base64_uploader :image, ImageRestaurantUploader
   belongs_to :restaurant, dependent: :destroy
   has_many :order_items,foreign_key: 'dish_id', dependent: :destroy
   validates_presence_of :name, :price, :description, :cooking_time
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,7 +3,7 @@ class Restaurant < ApplicationRecord
   has_many :dishes, dependent: :destroy
   belongs_to :user
   has_many :orders
-
+  mount_base64_uploader :image, ImageRestaurantUploader
   # validations
   validates_presence_of :name, :description
   validates :name, :description , presence: true
