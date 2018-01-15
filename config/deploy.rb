c069fe796f7247a0c8aaaf6f84f7262992c1b0bf
Orest Kostiuk <xxmolekula@gmail.com>
2018-01-15 15:29:54 +0200

    uploads

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :forward_agent, true     # SSH forward_agent.
 # Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
 # run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
 set :shared_dirs, fetch(:shared_dirs, []).push('log', 'public/system')
-set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml', 'config/application.yml')
+set :shared_files, fetch(:shared_files, []).push('config/database.yml','public/uploads', 'config/secrets.yml', 'config/application.yml')
 
 # This task is the environment that is loaded for all remote run commands, such as
 # `mina deploy` or `mina rake`.
