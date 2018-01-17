b7ebb4e76c5fdc5e3482f8b57f98a9d469f10b87
Orest Kostiuk <xxmolekula@gmail.com>
2018-01-17 15:25:53 +0200

    add restaurant id

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :phone, :provider, :uid, :name, :oauth_token, :token, :email, :image
+  attributes :id, :phone, :provider, :uid, :name, :oauth_token, :token, :email, :image, :restaurant
 
+  def restaurant
+   object.restaurants.first.id
+  end
   def image
         object.image
   end
