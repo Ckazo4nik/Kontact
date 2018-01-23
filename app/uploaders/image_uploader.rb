5b62b1c65f513f86463b072524b2c0f8d8cdd08b
Orest Kostiuk <xxmolekula@gmail.com>
2018-01-23 18:19:57 +0200

    add filename

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -15,15 +15,15 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   def filename
-    "#{Time.now.to_i}"
+
+    "#{Time.now.to_i}_#{@filename}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url(*args)
     # For Rails 3.1+ asset pipeline compatibility:
     # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-"jhbjhibibhjibkbh"
-    # "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+ "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   end
 
   # Process files as they are uploaded:
