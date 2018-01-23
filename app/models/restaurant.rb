737f85dbd1ee18189473e97c2865572ae350e0fd
Orest Kostiuk <xxmolekula@gmail.com>
2018-01-23 19:38:50 +0200

    fix

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,7 +3,7 @@ class Restaurant < ApplicationRecord
   has_many :dishes, foreign_key: 'restaurant_id', dependent: :destroy
   belongs_to :user
   has_many :orders
-v
+  after_save ThinkingSphinx::RealTime.callback_for(:restaurant)
   mount_base64_uploader :image, ImageRestaurantUploader
   # validations
   validates_presence_of :name, :description
