97587480c4e89ac69684cbd98c06b918b0ed75e0
Orest Kostiuk <xxmolekula@gmail.com>
2018-02-07 14:43:52 +0200

    update serializer

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,4 +1,4 @@
 class RestaurantSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :image, :user_id
+  attributes :id, :name, :description, :image, :user_id, :secret_key_restaurant, :public_key_restaurant
   has_many :dishes
 end
